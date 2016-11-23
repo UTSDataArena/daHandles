dd894987f68c1e6c11d35778608cb74ab04966a8
luke.cole <luke.cole@uts.edu.au>
2016-11-23 12:14:34 +1100

    Tidy unit test implementations.

--- tests/unit/pyhandles/cursor/pointer/test_MousePointerCursor.py
+++ tests/unit/pyhandles/cursor/pointer/test_MousePointerCursor.py
@@ -6,15 +6,12 @@ from pyhandles.cursor.pointer.MousePointerCursor import MousePointerCursor
 class MousePointerCursorTestCase(unittest.TestCase):
 
     def setUp(self):
-
         self.cursor = MousePointerCursor('id')
 
     def testConstruct(self):
-
         self.assertEqual('id', self.cursor.id)
 
     def testOnEvent(self):
-
         try:
             self.cursor.on_event()
         except NotImplementedError:
--- tests/unit/pyhandles/cursor/pointer/test_PointerCursor.py
+++ tests/unit/pyhandles/cursor/pointer/test_PointerCursor.py
@@ -6,15 +6,12 @@ from pyhandles.cursor.pointer.PointerCursor import PointerCursor
 class PointerCursorTestCase(unittest.TestCase):
 
     def setUp(self):
-
         self.cursor = PointerCursor('id')
 
     def testConstruct(self):
-
         self.assertEqual('id', self.cursor.id)
 
     def testOnEvent(self):
-
         try:
             self.cursor.on_event()
         except NotImplementedError:
