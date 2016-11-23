6da8d77887c733cf8b23624f44993368e536a6e7
luke.cole <luke.cole@uts.edu.au>
2016-11-23 13:01:02 +1100

    Add base implementation for controls.

---
+++ python/pyhandles/control/Control.py
@@ -0,0 +1,33 @@
+from pyhandles.selection.Selectable import Selectable
+
+
+class Control(Selectable):
+    """
+    Base class for all control implementations.
+    """
+
+    def __init__(self, parent):
+        super(Control, self).__init__()
+
+        self.parent = parent
+
+    def build(self):
+        raise NotImplementedError
+
+    def get_id(self):
+        super(Control, self).get_id()
+
+    def get_geo(self):
+        raise NotImplementedError
+
+    def set_visible(self, visible):
+        raise NotImplementedError
+
+    def match(self, candidate):
+        super(Control, self).match(candidate)
+
+    def on_select(self, context):
+        super(Control, self).on_select(context)
+
+    def on_release(self, context):
+        super(Control, self).on_release(context)
---
+++ tests/unit/pyhandles/control/test_Control.py
@@ -0,0 +1,46 @@
+import unittest
+
+from pyhandles.control.Control import Control
+
+
+class ControlTestCase(unittest.TestCase):
+
+    class MockParent(object):
+        pass
+
+    def setUp(self):
+        self.parent = ControlTestCase.MockParent()
+        self.control = Control(self.parent)
+
+    def testConstruct(self):
+        self.assertEqual(self.parent, self.control.parent)
+
+    def testBuild(self):
+        with self.assertRaises(NotImplementedError):
+            self.control.build()
+
+    def testGetId(self):
+        with self.assertRaises(NotImplementedError):
+            self.control.get_id()
+
+    def testGetGeo(self):
+        with self.assertRaises(NotImplementedError):
+            self.control.get_geo()
+
+    def testSetVisible(self):
+        with self.assertRaises(NotImplementedError):
+            self.control.set_visible(True)
+
+    def testSetMatch(self):
+        with self.assertRaises(NotImplementedError):
+            self.control.match(None)
+
+    def testOnSelect(self):
+        self.assertFalse(self.control.is_selected)
+        self.control.on_select(None)
+        self.assertTrue(self.control.is_selected)
+
+    def testOnRelease(self):
+        self.assertFalse(self.control.is_selected)
+        self.control.on_release(None)
+        self.assertFalse(self.control.is_selected)
