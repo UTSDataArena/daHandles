c2706a221f5cc458102ac7286239b74f1eb1c389
luke.cole <luke.cole@uts.edu.au>
2016-11-24 10:30:39 +1100

    Add setter for context property to base control builder.

--- python/pyhandles/control/ControlBuilder.py
+++ python/pyhandles/control/ControlBuilder.py
@@ -5,6 +5,7 @@ class ControlBuilder(object):
 
         self.id = None
         self.parent = None
+        self.context = None
 
     def set_id(self, id):
         self.id = id
@@ -12,6 +13,10 @@ class ControlBuilder(object):
 
     def set_parent(self, parent):
         self.parent = parent
+        return self
+
+    def set_context(self, context):
+        self.context = context
         return self
 
     def build(self):
