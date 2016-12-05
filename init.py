1b7469c108d0046436aab07952702edbde4d3d1d
luke.cole <luke.cole@uts.edu.au>
2016-12-06 10:37:57 +1100

    Reconfigure package to act as an omegalib module.

---
+++ init.py
@@ -0,0 +1,6 @@
+from context import *
+from control import *
+from cursor import *
+from cursor.controller import *
+from cursor.pointer import *
+from selection import *
--- python/daHandles/control/ControlGeometryBuilder.py
+++
@@ -1,7 +0,0 @@
-class ControlGeometryBuilder(object):
-
-    def __init__(self):
-        super(ControlGeometryBuilder, self).__init__()
-
-    def build(self):
-        raise NotImplementedError
