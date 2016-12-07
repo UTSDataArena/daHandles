180e2793c2ef68f5adaf1cc1fe9c4a4c86dd40c5
luke.cole <luke.cole@uts.edu.au>
2016-12-07 11:09:35 +1100

    Update module initialisation for compatibility with omegalib.

---
+++ cursor/__init__.py
@@ -0,0 +1,3 @@
+from .Cursor import Cursor
+from controller import *
+from pointer import *
---
+++ selection/__init__.py
@@ -0,0 +1,4 @@
+from .ControllableSceneNode import ControllableSceneNode
+from .Selectable import Selectable
+from .SelectableSceneNode import SelectableSceneNode
+from .SelectionManager import SelectionManager
