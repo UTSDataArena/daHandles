0634cc18d65aed622d9aa3bd0f98e13f19f85f5e
luke.cole <luke.cole@uts.edu.au>
2017-01-11 10:12:47 +1100

    Initial implementation of a houdini parameter control.

---
+++ control/HoudiniParameterControl.py
@@ -0,0 +1,98 @@
+from daHandles.control.GenericControl import GenericControl
+from daHandles.control.utility.Direction import Direction
+
+
+class HoudiniParameter:
+
+    HAPI_PARMTYPE_INT = 0
+    HAPI_PARMTYPE_MULTIPARM_LIST = 1
+    HAPI_PARMTYPE_TOGGLE = 2
+    HAPI_PARMTYPE_BUTTON = 3
+    HAPI_PARMTYPE_FLOAT = 4
+    HAPI_PARMTYPE_COLOR = 5
+    HAPI_PARMTYPE_STRING = 6
+    HAPI_PARMTYPE_PATH_FILE = 7
+    HAPI_PARMTYPE_PATH_FILE_GEO = 8
+    HAPI_PARMTYPE_PATH_FILE_IMAGE = 9
+    HAPI_PARMTYPE_PATH_NODE = 10
+    HAPI_PARMTYPE_FOLDERLIST = 11
+    HAPI_PARMTYPE_FOLDER = 12
+    HAPI_PARMTYPE_LABEL = 13
+    HAPI_PARMTYPE_SEPARATOR = 14
+
+    @staticmethod
+    def load_parameters(engine, asset_name):
+
+        wrapped = []
+        parameters = engine.loadParameters(asset_name)
+
+        for index in range(parameters.size()):
+            parameter = HoudiniParameter(engine, asset_name, parameters.getParameter(index))
+
+            if parameter.parentId >= 0:
+                parent = next((parent for parent in filter(
+                    lambda parent: parent.id == parameter.parentId, wrapped)), None)
+                if parent:
+                    parent.children.append(parameter)
+                else:
+                    raise HoudiniParameterException('Unable to find parent with id: %s' % parameter.parentId)
+            else:
+                wrapped.append(parameter)
+
+        return wrapped
+
+    def __init__(self, engine, asset_name, parameter=None):
+
+        self.engine = engine
+        self.asset_name = asset_name
+
+        self.id = parameter.getId() if parameter else None
+        self.parentId = parameter.getParentId() if parameter else None
+        self.name = parameter.getName() if parameter else None
+        self.label = parameter.getLabel() if parameter else None
+        self.type = parameter.getType() if parameter else None
+        self.size = parameter.getSize() if parameter else None
+
+        self.children = []
+
+    def set_value(self, value):
+
+        if self.type == HoudiniParameter.HAPI_PARMTYPE_INT:
+            self.engine.setIntegerParameter(self.asset_name, self.id, value)
+
+        elif self.type == HoudiniParameter.HAPI_PARMTYPE_FLOAT:
+            self.engine.setFloatParameter(self.asset_name, self.id, value)
+
+        elif self.type == HoudiniParameter.HAPI_PARMTYPE_STRING:
+            self.engine.setStringParameter(self.asset_name, self.id, value)
+
+        else:
+            raise HoudiniParameterException('Unable to set value on unsupported parameter type: %s' % self.type)
+
+
+class HoudiniParameterException(Exception):
+    pass
+
+
+class HoudiniParameterControl(GenericControl):
+
+    def __init__(self, id, parent, geometry_builder, ui_context):
+        super(HoudiniParameterControl, self).__init__(id, parent, geometry_builder, ui_context)
+
+        self.parameter = None
+        self.increment = None
+
+    def set_parameter(self, parameter):
+        self.parameter = parameter
+
+    def set_increment(self, increment):
+        self.increment = increment
+
+    def on_manipulate(self, position):
+        if self.parameter and self.increment:
+            direction = Direction.NEGATIVE if position.x <= self.position.x else Direction.POSITIVE
+
+            # TODO: we need to get the current value of the parameter (according to its type) add the
+            #       increment (multiplied by direction) and set it as the new parameter value - some
+            #       additional special handling will be needed depending on the parameter type (i.e.,
+            #       different behaviour will be necessary for string choice parameters etc).
