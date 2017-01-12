c672b452c5ca9c2c5cc927f72bc1c89e11dbece5
luke.cole <luke.cole@uts.edu.au>
2017-01-12 12:51:43 +1100

    Implement a sphere control geometry builder.

--- control/geometry/__init__.py
+++ control/geometry/__init__.py
@@ -1,3 +1,4 @@
 from .ControlGeometryBuilder import ControlGeometryBuilder
 from .CustomControlGeometryBuilder import CustomControlGeometryBuilder
 from .CylinderControlGeometryBuilder import CylinderControlGeometryBuilder
+from .SphereControlGeometryBuilder import SphereControlGeometryBuilder
