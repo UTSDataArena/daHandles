69bbbec7495515afdf104c76b8b49ba3ae58b26e
luke.cole <luke.cole@uts.edu.au>
2017-02-07 12:35:25 +1100

    Fix broken houdini parameter scale control group behaviour.

--- control/HoudiniParameterScaleControlGroupBuilder.py
+++ control/HoudiniParameterScaleControlGroupBuilder.py
@@ -9,9 +9,24 @@ class HoudiniParameterScaleControlGroupBuilder(ScaleControlGroupBuilder):
 
         self.parameter_mapping = {}
 
+        self.min_value = None
+        self.max_value = None
+
     def set_parameter_mapping(self, parameter_mapping):
         self.parameter_mapping = parameter_mapping
         return self
 
+    def set_min_value(self, min_value):
+        self.min_value = min_value
+        return self
+
+    def set_max_value(self, max_value):
+        self.max_value = max_value
+        return self
+
     def build(self):
-        return HoudiniParameterScaleControlGroup(self.parent, self.control_builder, self.bounding_box, self.ui_context, self.parameter_mapping)
+        control = HoudiniParameterScaleControlGroup(self.parent, self.control_builder, self.bounding_box, self.ui_context, self.parameter_mapping)
+        control.set_min_value(self.min_value)
+        control.set_max_value(self.max_value)
+
+        return control
